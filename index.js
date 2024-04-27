1469657a881a15ee72c4725cf82ad5058a708611
Mason Barnes <barnesmason980@gmail.com>
2024-04-26 18:26:55 -0700

    Lab Complete

---
+++ index.js
@@ -0,0 +1,30 @@
+function introduction(name){
+    return(`Hi, my name is ${name}.`);
+
+}
+
+introduction("Aki");
+
+
+function introductionWithLanguage(name, language){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+
+}
+
+introductionWithLanguage("Aki", "Ember.js");
+
+function introductionWithLanguageOptional(name, language){
+
+    if (language != "Python") {
+    return (`Hi, my name is ${name} and I am learning to program in JavaScript.`)}
+    else    
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+
+
+}
+    
+
+
+
+
+introductionWithLanguageOptional("Gracie","Python");
